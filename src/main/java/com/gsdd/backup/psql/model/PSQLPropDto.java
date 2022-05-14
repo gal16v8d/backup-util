df2a21ad5f10908ea3b259ff773475ff3036d3f5
Alex Galvis <alex.galvis.sistemas@gmail.com>
2022-05-14 00:16:03 -0500

    Switch from AOSP style

--- src/main/java/com/gsdd/backup/psql/model/PSQLPropDto.java
+++ src/main/java/com/gsdd/backup/psql/model/PSQLPropDto.java
@@ -5,15 +5,15 @@ import lombok.Data;
 @Data
 public class PSQLPropDto {
 
-    private String driverclass;
-    private String host;
-    private String port;
-    private String user;
-    private String pass;
-    private String dbName;
-    private String pgbin;
-    private String format;
-    private String type;
-    private String schema;
-    private String tables;
+  private String driverclass;
+  private String host;
+  private String port;
+  private String user;
+  private String pass;
+  private String dbName;
+  private String pgbin;
+  private String format;
+  private String type;
+  private String schema;
+  private String tables;
 }
